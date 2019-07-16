be3b83dcbea8ad5a21b73abe75276ff3f369a3c7
Peter <ruecktenwald@gmail.com>
2019-07-16 11:45:04 -0400

    added new and create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,5 +4,38 @@ class PortfoliosController < ApplicationController
     @portfolios = Portfolio.all
   end
 
+  def show
+  end
 
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to portfolios_path, notice: "Successfully created a new Portfolio!"
+    else
+      render new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle,:body)
+  end
 end
